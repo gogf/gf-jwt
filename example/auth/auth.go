f751b92b4acabe859b9014563132c16950489dcb
John <john@johng.cn>
2019-04-12 00:16:46 +0800

    up

--- example/auth/auth.go
+++ example/auth/auth.go
@@ -10,12 +10,19 @@ import (
 	"time"
 )
 
-type Default struct {
+var (
+	// The underlying JWT middleware.
 	GfJWTMiddleware *jwt.GfJWTMiddleware
-	Rules            g.Map
-}
+	// Customized login parameter validation rules.
+	ValidationRules = g.Map {
+		"username": "required",
+		"password": "required",
+	}
+)
 
-func (d *Default) Init() {
+// Initialization function,
+// rewrite this function to customized your own JWT settings.
+func init() {
 	authMiddleware, err := jwt.New(&jwt.GfJWTMiddleware{
 		Realm:           "test zone",
 		Key:             []byte("secret key"),
@@ -25,24 +32,26 @@ func (d *Default) Init() {
 		TokenLookup:     "header: Authorization, query: token, cookie: jwt",
 		TokenHeadName:   "Bearer",
 		TimeFunc:        time.Now,
-		Authenticator:   d.Authenticator,
-		LoginResponse:   d.LoginResponse,
-		RefreshResponse: d.RefreshResponse,
-		Unauthorized:    d.Unauthorized,
-		IdentityHandler: d.IdentityHandler,
-		PayloadFunc:     d.PayloadFunc,
+		Authenticator:   Authenticator,
+		LoginResponse:   LoginResponse,
+		RefreshResponse: RefreshResponse,
+		Unauthorized:    Unauthorized,
+		IdentityHandler: IdentityHandler,
+		PayloadFunc:     PayloadFunc,
 	})
 	if err != nil {
 		glog.Fatal("JWT Error:" + err.Error())
 	}
-	d.GfJWTMiddleware = authMiddleware
-	d.Rules           = g.Map {
-		"username": "required",
-		"password": "required",
-	}
+	GfJWTMiddleware = authMiddleware
 }
 
-func (d *Default) PayloadFunc(data interface{}) jwt.MapClaims {
+// PayloadFunc is a callback function that will be called during login.
+// Using this function it is possible to add additional payload data to the webtoken.
+// The data is then made available during requests via c.Get("JWT_PAYLOAD").
+// Note that the payload is not encrypted.
+// The attributes mentioned on jwt.io can't be used as keys for the map.
+// Optional, by default no additional data will be set.
+func PayloadFunc(data interface{}) jwt.MapClaims {
 	claims := jwt.MapClaims{}
 	params := data.(map[string]interface{})
 	if len(params) > 0 {
@@ -53,12 +62,14 @@ func (d *Default) PayloadFunc(data interface{}) jwt.MapClaims {
 	return claims
 }
 
-func (d *Default) IdentityHandler(r *ghttp.Request) interface{} {
+// IdentityHandler sets the identity for JWT.
+func IdentityHandler(r *ghttp.Request) interface{} {
 	claims := jwt.ExtractClaims(r)
 	return claims["id"]
 }
 
-func (d *Default) Unauthorized(r *ghttp.Request, code int, message string) {
+// Unauthorized is used to define customized Unauthorized callback function.
+func Unauthorized(r *ghttp.Request, code int, message string) {
 	r.Response.WriteJson(g.Map{
 		"code": code,
 		"msg":  message,
@@ -66,7 +77,8 @@ func (d *Default) Unauthorized(r *ghttp.Request, code int, message string) {
 	r.ExitAll()
 }
 
-func (d *Default) LoginResponse(r *ghttp.Request, code int, token string, expire time.Time) {
+// LoginResponse is used to define customized login-successful callback function.
+func LoginResponse(r *ghttp.Request, code int, token string, expire time.Time) {
 	r.Response.WriteJson(g.Map{
 		"code":   http.StatusOK,
 		"token":  token,
@@ -75,7 +87,8 @@ func (d *Default) LoginResponse(r *ghttp.Request, code int, token string, expire
 	r.ExitAll()
 }
 
-func (d *Default) RefreshResponse(r *ghttp.Request, code int, token string, expire time.Time) {
+// RefreshResponse is used to get a new token no matter current token is expired or not.
+func RefreshResponse(r *ghttp.Request, code int, token string, expire time.Time) {
 	r.Response.WriteJson(g.Map{
 		"code":   http.StatusOK,
 		"token":  token,
@@ -84,9 +97,12 @@ func (d *Default) RefreshResponse(r *ghttp.Request, code int, token string, expi
 	r.ExitAll()
 }
 
-func (d *Default) Authenticator(r *ghttp.Request) (interface{}, error) {
+// Authenticator is used to validate login parameters.
+// It must return user data as user identifier, it will be stored in Claim Array.
+// Check error (e) to determine the appropriate error message.
+func Authenticator(r *ghttp.Request) (interface{}, error) {
 	data := r.GetMap()
-	if e := gvalid.CheckMap(data, d.Rules); e != nil {
+	if e := gvalid.CheckMap(data, ValidationRules); e != nil {
 		return "", jwt.ErrFailedAuthentication
 	}
 	if data["username"] == "admin" && data["password"] == "admin" {
@@ -95,6 +111,5 @@ func (d *Default) Authenticator(r *ghttp.Request) (interface{}, error) {
 			"id":       data["username"],
 		}, nil
 	}
-
 	return nil, jwt.ErrFailedAuthentication
 }
--- example/server/server.go
+++ example/server/server.go
@@ -6,28 +6,32 @@ import (
 	"github.com/gogf/gf/g/net/ghttp"
 )
 
+// hello should be authenticated to view.
 func hello(r *ghttp.Request) {
 	r.Response.Write("Hello World!")
 }
 
+// works is the default router handler for web server.
+func works(r *ghttp.Request) {
+	r.Response.Write("It works!")
+}
+
+// authHook is the HOOK function implements JWT logistics.
+func authHook(r *ghttp.Request) {
+	r.Response.CORSDefault()
+	auth.GfJWTMiddleware.MiddlewareFunc()(r)
+}
+
 func main() {
 	s := g.Server()
-	a := new(auth.Default)
-	a.Init()
-
-	s.BindHandler("/login", a.GfJWTMiddleware.LoginHandler)
-
-	s.Group("/user").Bind("/user", []ghttp.GroupItem{
-		{"ALL", "*", func(r *ghttp.Request) {
-			r.Response.CORSDefault()
-			a.GfJWTMiddleware.MiddlewareFunc()(r)
-		}, ghttp.HOOK_BEFORE_SERVE},
-		{"GET", "/refresh_token", a.GfJWTMiddleware.RefreshHandler},
-		{"GET", "/hello", hello},
+	s.Group().Bind("/", []ghttp.GroupItem{
+		{"ALL",  "/",             works},
+		{"POST", "/login",        auth.GfJWTMiddleware.LoginHandler},
 	})
-
-	s.BindHandler("/", func(r *ghttp.Request) {
-		r.Response.Write("It works!")
+	s.Group("/user").Bind("/user", []ghttp.GroupItem{
+		{"ALL", "*",              authHook, ghttp.HOOK_BEFORE_SERVE},
+		{"GET", "/refresh_token", auth.GfJWTMiddleware.RefreshHandler},
+		{"GET", "/hello",         hello},
 	})
 	s.SetPort(8000)
 	s.Run()
